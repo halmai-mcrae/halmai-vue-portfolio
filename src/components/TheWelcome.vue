769d32ff87bdd6dec3a77e0778ff880187f935f1
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-06-27 13:52:47 +1200

    Gix manifest issues in applications tab

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -48,8 +48,7 @@ Manu Mana is more than a web application: the project is about design, protectin
     </template>
     <template #heading>Another project</template>
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-    sunt in culpa qui officia deserunt mollit anim id est laborum.
+    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
   </WelcomeItem>
 
   <WelcomeItem>
@@ -58,9 +57,9 @@ Manu Mana is more than a web application: the project is about design, protectin
       </RouterLink>
   <RouterView />
     </template>
-    <!-- <template #heading>More projects</template>
+    <template #heading>More projects</template>
 
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. -->
+    tempor incididunt ut labore et dolore magna aliqua.
   </WelcomeItem>
 </template>
